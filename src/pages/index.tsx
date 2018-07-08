28231c37fdd46d3fc294f47b8efdaafe0da3e907
ismail codar <icodar@gmail.com>
2018-07-09 01:30:45 +0300

    Experimental state management with withPureComponent, createCompoent HOC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,12 @@ import {
   DialogContentText,
   DialogActions,
   Button,
-  Typography
+  Typography,
+  Input,
+  InputLabel
 } from "@material-ui/core";
 import { Provider, Subscribe } from "react-contextual";
+import { createComponent, withPureComponent } from "../hoc/component";
 
 const styles: StyleRulesCallback<"root"> = theme => ({
   root: {
@@ -22,16 +25,19 @@ const styles: StyleRulesCallback<"root"> = theme => ({
 });
 
 const State = {
-  open: false,
+  open: false
+};
+
+const StateHandlers = {
   handleClick: () => state => ({ open: true }),
   handleClose: () => state => ({ open: false })
 };
 
 const Index = (props: any) => {
   return (
-    <Provider {...State}>
+    <Provider {...{ ...State, ...StateHandlers }}>
       <Subscribe>
-        {(state: typeof State) => {
+        {(state: typeof State & typeof StateHandlers) => {
           return (
             <div className={props.classes.root}>
               <Dialog open={state.open} onClose={state.handleClose}>
@@ -66,4 +72,74 @@ const Index = (props: any) => {
   );
 };
 
-export default withRoot(withStyles(styles)(Index));
+// export default withRoot(withStyles(styles)(Index));
+
+const Counter = withPureComponent(
+  { count: 0 },
+  {
+    handleIncrease: state => ({ count: state.count + 1 }),
+    handleDecrease: state => ({ count: state.count - 1 })
+  },
+  props => (
+    <>
+      <Button onClick={props.handlers.handleDecrease} mini>
+        -
+      </Button>
+      {props.state.count}
+      <Button onClick={props.handlers.handleIncrease} mini>
+        +
+      </Button>
+    </>
+  )
+);
+
+const CounterParent = withPureComponent(
+  { startCount: 0 },
+  {
+    handleChangeStart: (state, binded, e) => {
+      return {
+        startCount: parseInt(e.target.value)
+      };
+    }
+  },
+  props => (
+    <div>
+      <InputLabel>
+        Start:{" "}
+        <Input
+          type="number"
+          value={props.state.startCount}
+          onChange={props.handlers.handleChangeStart}
+        />
+      </InputLabel>
+      <br />
+      <Counter count={props.state.startCount} />
+    </div>
+  )
+);
+
+const Index2 = withPureComponent(
+  {
+    open: false
+  },
+  {
+    // handleClick: () => state => ({ open: true }),
+    // handleClose: () => state => ({ open: false })
+    handleClick: state => ({ open: true }),
+    handleClose: state => ({ open: false })
+  },
+  state => {
+    return (
+      <div className={state.classes.root}>
+        <CounterParent startCount={0} />
+      </div>
+    );
+  },
+  theme => ({
+    root: {
+      textAlign: "center",
+      paddingTop: theme.spacing.unit * 20
+    }
+  })
+);
+export default withRoot(Index2);
