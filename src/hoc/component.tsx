a9235d2b6736bdcbaa406458b0146fab9095782b
ismail codar <icodar@gmail.com>
2018-07-09 14:24:01 +0300

    createComponent improvement

--- src/hoc/component.tsx
+++ src/hoc/component.tsx
@@ -1,22 +1,18 @@
 import * as React from "react";
-import { Theme, withStyles } from "@material-ui/core";
-import { CSSProperties } from "@material-ui/core/styles/withStyles";
+import { withStyles, StyleRulesCallback } from "@material-ui/core";
 import { Provider, Subscribe } from "react-contextual";
-
-export type CssType = { [key: string]: CSSProperties };
-export type ThemedCssType = (theme: Theme) => CssType;
 
 export const createComponent = <
   S,
   H extends {
     [key: string]: (e?: any) => (state: S) => Partial<S> | Promise<Partial<S>>;
   },
-  C extends ThemedCssType
+  C extends StyleRulesCallback<string>
 >(
   state: S,
   handlers: H,
   view: (
-    stateWithHandlers: S & H,
+    store: S & H,
     props?: { classes: { [key in keyof ReturnType<C>]: any } }
   ) => any,
   classes?: C
@@ -31,8 +27,8 @@ export const createComponent = <
       return Object.assign({}, props, store);
     };
     const viewFn = ((props, view) => {
-      return stateWithHandlers => {
-        return view(stateWithHandlers, props);
+      return store => {
+        return view(store, props);
       };
     })(props, view);
     return (
@@ -48,136 +44,4 @@ export const createComponent = <
         name: ""
       })(returnedView)
     : (returnedView as any);
-};
-
-const specialReactProps = ["children", "state", "handlers", "$load"];
-export const isPromise = obj => {
-  return (
-    !!obj &&
-    (typeof obj === "object" || typeof obj === "function") &&
-    typeof obj.then === "function"
-  );
-};
-
-export const initComponent = (self, initState, handlers) => {
-  const handlersOut = { $load: null };
-  const callback = data => {
-    data && self.setState(data);
-  };
-  //$load in çalışmaması istendiğinde $load={null} verilebilir
-  if (self.props.$load !== undefined) handlers.$load = self.props.$load;
-  for (var key in handlers) {
-    (function(key, handler) {
-      if (handler)
-        handlersOut[key] = function(...args) {
-          const ret = this
-            ? handler(self.state, this, ...args)
-            : handler(self.state, ...args);
-          if (isPromise(ret)) {
-            ret.then(data => callback(data));
-          } else callback(ret);
-        };
-    })(key, handlers[key]);
-  }
-
-  self.handlers = handlersOut;
-  if (self.props.handlers) {
-    // props.handlers.xxxMethod çağrılacaksa handlers={{} as any} şeklinde atama gerekir
-    Object.assign(self.props.handlers, handlersOut);
-  }
-  // self.state = {
-  //   ...self.state,
-  //   ...(initState ? initState(self.props) : null),
-  //   ...self.props
-  // };
-  self.state = Object.assign(
-    {},
-    self.state,
-    initState ? initState(self.props) : null,
-    self.props
-  );
-  if (handlersOut.$load) handlersOut.$load(self.state);
-};
-
-export const withPureComponent = <
-  S,
-  H extends {
-    [key: string]: (state: S, ...args) => Partial<S> | Promise<Partial<S>>;
-  },
-  C extends ThemedCssType
->(
-  state: S,
-  handlers: H,
-  view: (
-    props: {
-      state: S;
-      handlers: { [key in keyof H]: (e) => any };
-      classes: { [key in keyof ReturnType<C>]: any };
-    }
-  ) => any,
-  classes?: C,
-  options?: {
-    receiveProps?: any;
-    initState?: (params: S) => Partial<S>;
-  }
-): React.ComponentType<S> => {
-  const View = classes
-    ? withStyles(classes, {
-        withTheme: true,
-        name: ""
-      })(view)
-    : view;
-
-  return class extends React.PureComponent<S, S> {
-    classes: any;
-    handlers: any;
-    state = state;
-    constructor(props) {
-      super(props);
-      initComponent(
-        this,
-        options ? options.initState : null,
-        Object.assign({}, handlers) //Object. assign ile vermek mutlak gerekli
-      );
-    }
-    // static getDerivedStateFromProps(props, state) {
-    //   console.log(props, state);
-    //   if (options && options.receiveProps) {
-    //     return options.receiveProps(props, state);
-    //   } else {
-    //     if (state !== props) {
-    //       var _state = {};
-    //       for (var key in props) {
-    //         if (state[key] !== props[key] && !specialReactProps.includes(key)) {
-    //           _state[key] = props[key];
-    //         }
-    //       }
-    //       return _state;
-    //     }
-    //   }
-    //   return null;
-    // }
-    UNSAFE_componentWillReceiveProps(nextProps) {
-      if (options && options.receiveProps) {
-        options.receiveProps(this.state, nextProps);
-      } else {
-        if (this.props !== nextProps) {
-          for (var key in nextProps) {
-            if (
-              this.props[key] !== nextProps[key] &&
-              !specialReactProps.includes(key)
-            ) {
-              //güncelleme olmuyorsa nextProps[key] object dir ve instance değişmiyordur
-              this.state[key] = nextProps[key];
-            }
-          }
-        }
-      }
-    }
-    render() {
-      return (
-        <View {...this.props} state={this.state} handlers={this.handlers} />
-      );
-    }
-  };
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,145 +3,8 @@ import withStyles, {
   StyleRulesCallback
 } from "@material-ui/core/styles/withStyles";
 import withRoot from "../withRoot";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-  Button,
-  Typography,
-  Input,
-  InputLabel
-} from "@material-ui/core";
-import { Provider, Subscribe } from "react-contextual";
-import { createComponent, withPureComponent } from "../hoc/component";
-import { POINT_CONVERSION_UNCOMPRESSED } from "constants";
-
-const styles: StyleRulesCallback<"root"> = theme => ({
-  root: {
-    textAlign: "center",
-    paddingTop: theme.spacing.unit * 20
-  }
-});
-
-/*
-const Counter = withPureComponent(
-  { count: 0 },
-  {
-    handleIncrease: state => ({ count: state.count + 1 }),
-    handleDecrease: state => ({ count: state.count - 1 })
-  },
-  props => (
-    <>
-      <Button onClick={props.handlers.handleDecrease} mini>
-        -
-      </Button>
-      {props.state.count}
-      <Button onClick={props.handlers.handleIncrease} mini>
-        +
-      </Button>
-    </>
-  )
-);
-
-const CounterParent = withPureComponent(
-  { startCount: 0 },
-  {
-    handleChangeStart: (state, binded, e) => {
-      return {
-        startCount: parseInt(e.target.value)
-      };
-    }
-  },
-  props => (
-    <div>
-      <InputLabel>
-        Start:{" "}
-        <Input
-          type="number"
-          value={props.state.startCount}
-          onChange={props.handlers.handleChangeStart}
-        />
-      </InputLabel>
-      <br />
-      <Counter count={props.state.startCount} />
-    </div>
-  )
-);
-
-
-const counterStore = {
-  count: 0,
-  internal: false,
-  handleIncrease: () => state => ({ count: state.count + 1 }),
-  handleDecrease: () => state => ({ count: state.count - 1 })
-};
-
-const counterParentStore = {
-  startCount: 0,
-  handleChangeStart: e => state => {
-    return {
-      startCount: parseInt(e.target.value)
-    };
-  }
-};
-
-const Counter = props => {
-  let fromProps = true;
-  const select = store => {
-    if (fromProps) {
-      fromProps = false;
-      return Object.assign(store, props);
-    }
-    return Object.assign({}, props, store);
-  };
-  return (
-    <Provider {...counterStore}>
-      <Subscribe select={select}>
-        {(state: typeof counterStore) => {
-          return (
-            <>
-              <Button onClick={state.handleDecrease} mini>
-                -
-              </Button>
-              {state.count}
-              <Button onClick={state.handleIncrease} mini>
-                +
-              </Button>
-            </>
-          );
-        }}
-      </Subscribe>
-    </Provider>
-  );
-};
-
-const CounterParent = props => {
-  return (
-    <Provider {...counterParentStore}>
-      <Subscribe>
-        {(state: typeof counterParentStore) => {
-          return (
-            <div>
-              <InputLabel>
-                Start:{" "}
-                <Input
-                  type="number"
-                  value={state.startCount}
-                  onChange={state.handleChangeStart}
-                />
-              </InputLabel>
-              <br />
-              <Counter count={state.startCount} />
-            </div>
-          );
-        }}
-      </Subscribe>
-    </Provider>
-  );
-};
-*/
+import { Button, Input, InputLabel } from "@material-ui/core";
+import { createComponent } from "../hoc/component";
 
 const Counter = createComponent(
   { count: 0 },
@@ -149,13 +12,13 @@ const Counter = createComponent(
     handleIncrease: () => state => ({ count: state.count + 1 }),
     handleDecrease: () => state => ({ count: state.count - 1 })
   },
-  state => (
+  store => (
     <>
-      <Button onClick={state.handleDecrease} mini>
+      <Button onClick={store.handleDecrease} mini>
         -
       </Button>
-      {state.count}
-      <Button onClick={state.handleIncrease} mini>
+      {store.count}
+      <Button onClick={store.handleIncrease} mini>
         +
       </Button>
     </>
@@ -165,50 +28,47 @@ const Counter = createComponent(
 const CounterParent = createComponent(
   { startCount: 0 },
   {
-    handleChangeStart: e => state => {
-      return {
-        startCount: parseInt(e.target.value)
-      };
-    }
+    handleChangeStart: e => state => ({
+      startCount: parseInt(e.target.value)
+    })
   },
-  state => (
+  store => (
     <div>
       <InputLabel>
         Start:{" "}
         <Input
           type="number"
-          value={state.startCount}
-          onChange={state.handleChangeStart}
+          value={store.startCount}
+          onChange={store.handleChangeStart}
         />
       </InputLabel>
       <br />
-      <Counter count={state.startCount} />
+      <Counter count={store.startCount} />
     </div>
   )
 );
 
-const Index = withPureComponent(
+const Index = createComponent(
+  {},
   {
-    open: false
+    handleClick: () => state => ({ open: true }),
+    handleClose: () => state => ({ open: false })
   },
-  {
-    // handleClick: () => state => ({ open: true }),
-    // handleClose: () => state => ({ open: false })
-    handleClick: state => ({ open: true }),
-    handleClose: state => ({ open: false })
-  },
-  state => {
+  (store, props) => {
     return (
-      <div className={state.classes.root}>
+      <div className={props.classes.root}>
         <CounterParent startCount={0} />
       </div>
     );
   },
-  theme => ({
+  (theme => ({
     root: {
       textAlign: "center",
       paddingTop: theme.spacing.unit * 20
     }
-  })
+  })) as StyleRulesCallback<"root">
 );
 export default withRoot(Index);
+
+declare type C = (theme) => { root: null };
+const classes: { [key in keyof ReturnType<C>]: any } = null;
